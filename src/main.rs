ddedca1a6e2df32faa65b1662e8d2db1738f2ce7
chadat23 <chad.derosier@tutanota.com>
2022-04-09 16:35:46 -0700

    Kinda finding widgets.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
 use gui_rs::guiprocessing;
-use gui_rs::guiresources::GUIResources;
-use gui_rs::guiwidgets::{GUIBase, GUIWindow};
+use gui_rs::guiproperties::GUIColor;
+use gui_rs::guiproperties::guiposition::{GUISize, GUIPosition, GUILength};
+use gui_rs::guiproperties::guitraits::{Parent, Widget};
+use gui_rs::guiresources::{GUIResources, GUIBackend};
+use gui_rs::guiwidgets::{GUIBase, GUIWindow, GUIButton};
 
 fn main() {
     let mut guibase = GUIBase::new();
-    // guibase::new_window();
-    let window = GUIWindow::new(guibase.next_window_id());
-    guibase.add_window(window);
+
+    let window = GUIWindow::default();
+
+    let button_one = GUIButton::default();
+
+    let window_id = guibase.add_window(window);
+    let button_id = guibase.add_child_to_parent(button_one, window_id);
+
+    let mut button_two = GUIButton::default();
+    button_two.size = GUISize::from_pixels(20., 20.);
+    button_two.radius = GUILength::from_pixels(5.);
+    button_two.position = GUIPosition::from_pixels(10., 10.);
+    button_two.background_color = GUIColor::from_rgba_u8u8u8u8(255, 0, 0, 255);
+    guibase.add_child_to_parent(button_two, window_id);
+
+    let mut button_three = GUIButton::default();
+    button_three.size = GUISize::from_pixels(120., 200.);
+    button_three.position = GUIPosition::from_pixels(100., 100.);
+    button_three.background_color = GUIColor::from_rgba_u8u8u8u8(0, 150, 150, 255);
+    guibase.add_child_to_parent(button_three, window_id);
+
 
     let resources = GUIResources::default();
 
